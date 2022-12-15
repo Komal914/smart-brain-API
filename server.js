46e5c0fc82ef08e480e5c414133eb073adb6da14
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2022-12-14 21:42:33 -0500

    added route to home

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require("express");
 //calling express on our application
 const app = express();
 
+//creating routes
+app.get("/", (req, res) => {
+  res.send("this is home");
+});
+
 //listening on our port
 app.listen(3000, () => {
   console.log("i am running");
