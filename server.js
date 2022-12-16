fc3b190190683cc2420af1bbe37f45208de4db69
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2022-12-15 22:21:48 -0500

    server.js updated

--- server.js
+++ server.js
@@ -4,9 +4,39 @@ const express = require("express");
 //calling express on our application
 const app = express();
 
+//our database
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "john",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "Sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 //creating routes
 app.get("/", (req, res) => {
   res.send("this is home");
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  )
+    res.json("signin");
 });
 
 //listening on our port
