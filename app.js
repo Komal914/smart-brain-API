f76d83ad74e889ad92ca52f8396afd958b19238f
Komalpreet Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2022-12-25 16:29:43 -0500

    adding databe connection

--- app.js
+++ app.js
@@ -15,10 +15,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-flat-62649",
-    user: "ufdso2gmpua6lj",
-    password: "",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
